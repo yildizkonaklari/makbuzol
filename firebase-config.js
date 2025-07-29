fd122d63de734d144a4c0eaceb901f8abf12f9de
yildizkonaklari <ordulusel@gmail.com>
2025-07-29 09:42:50 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Firebase projenizin yapılandırma bilgileri buraya gelecek
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Firebase'i başlat
+firebase.initializeApp(firebaseConfig);
+
+// Firebase servislerini dışa aktar
+const auth = firebase.auth();
+const db = firebase.firestore();
